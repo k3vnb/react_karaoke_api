fc8dd5199f333be4412866dfdb40ffd04a19e770
Kevin Boyle <papershack@gmail.com>
2018-02-26 06:51:10 -0800

    add new action & test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import * as types from "./../constants/ActionTypes";
+import v4 from 'uuid/v4';
 
 export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
@@ -17,3 +18,23 @@ export const changeSong = (newSelectedSongId) => ({
 });
 
 //newSelectedSongId is the payload used by its corresponding reducer
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE').then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+    });
+  };
+}
+
+export const requestSong = (title, localSongId) => ({
+  type: types.REQUEST_SONG,
+  title,
+  songId: localSongId
+});
